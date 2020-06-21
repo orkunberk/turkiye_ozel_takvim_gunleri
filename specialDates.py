c37490f2f18654f8afef9dda4620640a66893d49
Orkun Berk Yüzbaşıoğlu <orkunberk@hotmail.com>
2020-06-21 16:32:19 +0300

    Update specialDates.py
    takvim baslangic ve bitise baglandi

--- specialDates.py
+++ specialDates.py
@@ -1,41 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 26 16:18:09 2020
+Created on Sun Jun 21 16:31:17 2020
 
 @author: orkun.yuzbasioglu
 """
 
-#Special Dates
+#Ozel takvim gunleri
+
 import pandas as pd
 import datetime
 
 from workalendar.europe import Turkey
 from itertools import chain
 
-cal = Turkey()
-
-#special_dates = list((chain([cal.holidays(year) for year in range(2018, 2021)])))
-special_dates = [cal.holidays(year) for year in range(2016, 2021)]
+#Dataframe icin baslangic ve bitis tarihleri
+baslangic = 2015
+bitis = 2020
 
 dates = []
 events = []
 
-dates = list(chain(*[[day[0] for day in year] for year in special_dates]))
-events = list(chain(*[[day[1] for day in year] for year in special_dates]))
-special_dates_df = pd.DataFrame({'Date':dates, 'event':events})
+cal = Turkey()
+
+special_dates = [cal.holidays(year) for year in range(baslangic, bitis+1)]
+
+dates = [el[0] for el in list(chain(*special_dates))]
+events= [el[1] for el in list(chain(*special_dates))]
+
+special_dates_df = pd.DataFrame({'Date':dates, 'Event':events})
 special_dates_df['Date'] =  pd.to_datetime(special_dates_df['Date'])
 
-''' 20.03
-special_dates_final = special_dates_df.copy()
-special_dates_final['event'] = 1
-special_dates_final['event']  = pd.to_numeric(special_dates_final['event'])
-'''
+calender = pd.DataFrame({'Date':pd.date_range(start='1/1/'+str(baslangic), end='3/15/'+str(bitis))})
 
-#Dates processing
-calender = pd.DataFrame({'Date':pd.date_range(start='1/1/2016', end='3/15/2020')})
-#calender_df = calender.merge(special_dates_final, how='left').fillna(0)
-calender_df = calender.merge(special_dates_df, how='left').fillna('NotASpecialDate')
-calender_df.set_index('Date', inplace=True)
-calenderHourly = calender_df.resample('60min').ffill()
+calender_df = calender.merge(special_dates_df, how='left').fillna('OzelGunDegil')
 
-#calenderHourly.drop(calenderHourly.tail(1).index, inplace=True)
+calender_df.head()
+calender_df.tail()
